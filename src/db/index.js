3d70126435f3842211e0c3af531575effa214f69
Pritam <kumarpreetjangir@gmail.com>
2025-11-03 23:43:54 +0530

    first commit

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js";
+;
+
+const connectDB = async ()=>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n mongoDB connected !! DB HOST: ${connectionInstance}`)
+    } catch (error) {
+        console.log("ERROR in connectiong with db",error);
+        process.exit(1);
+    }
+}
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,37 @@
+// require('dotenv').config()
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+connectDB()
+
+
+
+
+
+
+
+
+
+
+/*
+import express from 'express'
+const app = express()
+( async() =>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("Error in conncecting with db",error)
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on the port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log("ERROR: ",error)
+        throw err
+    }
+})()
+
+*/
