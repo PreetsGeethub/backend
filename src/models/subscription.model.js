36a708aff0684b0c14320b9129765d9702b41f6f
Pritam <kumarpreetjangir@gmail.com>
2025-11-26 22:46:07 +0530

    add subscription controllers

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,7 +1,7 @@
 import mongoose,{Schema} from "mongoose"
 
 const subscriptionSchema = new Schema({
-    subscribers: {
+    subscriber: {
         type: Schema.Types.ObjectId, // one who is subscribing
         ref: "User",
     },
@@ -10,6 +10,7 @@ const subscriptionSchema = new Schema({
         ref: "User",
     },
 },{timestamps: true})
+
 
 
 export const  Subscription = mongoose.model("Subscription",subscriptionSchema)
